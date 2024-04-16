670ebaede2e6448276b30ab5b4dcd429b91c1ad5
lemongoose228 <150834453+lemongoose228@users.noreply.github.com>
2024-04-16 11:03:05 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='project',
+    version='1.0',
+    packages=find_packages(),
+    install_requires=['numpy'],  # Пример зависимости
+    author='Kate Konova',
+    author_email='konova.ek@mail.ru',
+    description='calculates the area of ​​a circle given a given radius and is used to check if a number is prime',
+    url='',
+    extras_require={
+          'test': [
+              'pytest',
+              'coverage',
+          ],
+     },
+    license='MIT',
+)
