781aed32ef9ab0306fa1df0acde7ce26549a7a08
lemongoose228 <150834453+lemongoose228@users.noreply.github.com>
2024-04-16 11:20:48 +0800

    Update main.py

--- code/main.py
+++ code/main.py
@@ -1,12 +1,12 @@
-import math
+import numpy
 
 def calculate_circle_area(radius):
-    return math.pi * radius**2
+    return numpy.pi * radius**2
 
 def is_prime(n):
     if n <= 1:
         return False
-    for i in range(2, int(math.sqrt(n)) + 1):
+    for i in range(2, int(numpy.sqrt(n)) + 1):
         if n % i == 0:
             return False
     return True
