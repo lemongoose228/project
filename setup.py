4c6745e49228d75011044edd31c8157f1050e012
lemongoose228 <150834453+lemongoose228@users.noreply.github.com>
2024-04-17 18:36:21 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name='project',
     version='1.0',
-    packages=find_packages(),
-    install_requires=['numpy'],  # Пример зависимости
+     packages=find_packages(exclude=['test']),
+    install_requires=['numpy'],  
     author='Kate Konova',
     author_email='konova.ek@mail.ru',
     description='calculates the area of ​​a circle given a given radius and is used to check if a number is prime',
